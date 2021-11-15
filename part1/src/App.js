07a5e783fe2a24981add87efa3c9a577bfdc970c
Gonzalo Gómez <gonzagom66@yahoo.com.ar>
2021-11-15 12:08:14 -0300

    react

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,50 +1,59 @@
 import './App.css';
+import { useState } from 'react';
 // import Content from './Content';
 // import Header from './Header.jsx';
 // import Total from './Total';
 
-// function App() {
-//   const course = 'Half Stack application development'
-//   const part1 = 'Fundamentals of React'
-//   const exercises1 = 10
-//   const part2 = 'Using props to pass data'
-//   const exercises2 = 7
-//   const part3 = 'State of a component'
-//   const exercises3 = 14
-
-//   return (
-//     <>
-//     <Header course={course} />
-//     <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-//     <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-//     </>
-//   );
-// }
-
-const course = 'Half Stack application development'
-const parts = [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }
-]
-
 const App = () => {
-  
+  // const contador = useState(0)
+  // const contadorValue = contador[0]
+  // const updateContador = contador[1]
+  const [contador, updateContador] = useState(0)
+  const handleClickIncrement = () => {
+    updateContador(contador + 1)
+  }
+  const handleClickDecrement = () => {
+    updateContador(contador - 1)
+  }
+  const handleClickReset = () => {
+    // updateContador(contador - contador)
+    updateContador(0)
+  }
+  // setInterval(() => {
+  //   updateContador(contadorValue+1)
+  // }, 2000)
+  // const course = 'Half Stack application development'
+  // const part1 = 'Fundamentals of React'
+  // const exercises1 = 10
+  // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+  // const part3 = 'State of a component'
+  // const exercises3 = 14
+
+  // return (
+  //   <>
+  //   <Header course={course} />
+  //   <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+  //   <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+  //   </>
+  // );
   return (
-    <div>
-      <h1>{course}</h1>
-      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-    </div>
+    <>
+      <h1>{contador}</h1>
+      <button onClick={ handleClickIncrement
+        // () => { 
+        // updateContador(contador + 1)
+        // updateContador(prevContador => {
+        //   return prevContador + 1
+        // })
+        // }
+        }>Incrementar</button>
+        <button onClick={ handleClickDecrement }>Decrementar</button>
+        <button onClick={ handleClickReset }>Reset</button>
+
+    </>
   )
+
 }
 
 export default App;
