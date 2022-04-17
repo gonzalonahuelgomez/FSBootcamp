97191568e311cd4d19ce131ff2460761e9415b09
Gonzalo Gómez <gonzagom66@yahoo.com.ar>
2022-04-17 19:01:36 -0300

    TJ Jira

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,54 +1,8 @@
-import { useState } from 'react'
-import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
-
-let CONTADOR = 4
-
 const App = () => {
+ 
   
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
-
-  const addPerson = (event) => {
-    event.preventDefault()
-    const personObject = {
-      id: ++CONTADOR,
-      name: newName,
-      number: newNumber,
-    }
-    persons.map(person => person.name).includes(newName) ? alert(`${newName} is already added to phonebook`) : 
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
-  }
-  
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value)
-  }
-  
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter name={filter} onChange={handleFilterChange}/>
-      <h2>add a new</h2>
-      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
-      <h2>Numbers</h2>
-      <Persons persons={persons} filter={filter}/>
-    </div>
+  return(
+    <></>
   )
 }
 
