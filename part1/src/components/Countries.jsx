a22263f280c5efc979fa6c63d1eefbf451c25b6b
Gonzalo Gómez <gonzagom66@yahoo.com.ar>
2022-04-21 13:20:10 -0300

    finished 2c

--- part1/src/components/Countries.jsx
+++ part1/src/components/Countries.jsx
@@ -3,8 +3,7 @@ import Country from "./Country"
 
 const Countries = ({countries, filter}) => {
     let filterCountry = countries.filter((country) => country.name.common.toLowerCase().includes(filter.toLowerCase())) //country.name.common.toLowerCase().indexOf(filter.toLowerCase()) !== -1)
-    const handleClick = (country) =>{<Country key={country.name.common} country={country} /> 
-        }
+    const handleClick = (country) => console.log(country)
     if(filterCountry.length > 10) return <p>Too many matches, specify another filter</p>
     else if(filterCountry.length > 1) return filterCountry.map((filteredCountry, i) => <li key={i}>{filteredCountry.name.common}<Boton text={"show"} handleClick={()=>handleClick(filteredCountry)} /></li>)
     else return filterCountry.map((filteredCountry, i) => <Country key={i} country={filteredCountry} />)
--- part1/src/components/Country.jsx
+++ part1/src/components/Country.jsx
@@ -1,3 +1,5 @@
+import Weather from "./Weather"
+
 const Country = ({country}) => {
   return (
     <div>            
@@ -8,6 +10,7 @@ const Country = ({country}) => {
         {/* {Object.values(country.languages).map(language => <li key={language}>{language}</li>)} */}
         {Object.entries(country.languages).map(([key,language]) => <li key={key}>{language}</li>)}
         <img src={country.flags.png} alt={`${country.name.common} flag`}/>
+        <Weather capital={country.capital} /*lat={country.capitalInfo.latlng[0]} lng={country.capitalInfo.latlng[1]}*//>
     </div>
   )
 }
