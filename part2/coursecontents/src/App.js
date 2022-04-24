9413d588e5409ec5a2328158a6b66cf56cc0d98c
Gonzalo Gómez <gonzagom66@yahoo.com.ar>
2022-04-23 23:34:24 -0300

    TJ-5 module 2e

--- part2/coursecontents/src/App.js
+++ part2/coursecontents/src/App.js
@@ -6,6 +6,7 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService
@@ -14,6 +15,18 @@ const App = () => {
         setNotes(initialNotes)
       })
   }, [])
+
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+  
+    return (
+      <div className='error'>
+        {message}
+      </div>
+    )
+  }
 
   const addNote = (event) => {
     event.preventDefault()
@@ -40,20 +53,29 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
-    const toggleImportanceOf = (id) => {
-      const note = notes.find(n => n.id === id)
-      const changedNote = { ...note, important: !note.important }
-    
-      noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(changedNote).then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
-    }
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
 
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
