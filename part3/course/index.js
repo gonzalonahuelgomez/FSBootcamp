56628814e82ccdf3f356906a0eb83a9d91ab6d4a
Gonzalo Gómez <gonzagom66@yahoo.com.ar>
2022-04-29 23:58:02 -0300

    TJ-7 node

--- part3/course/index.js
+++ part3/course/index.js
@@ -21,6 +21,8 @@ let notes = [
       important: true
     }
   ]
+
+  app.use(express.json())
   
   app.get('/', (request, response) => {
     response.send('<h1>Hello Asd!</h1>')
@@ -38,6 +40,34 @@ let notes = [
     } else {
       response.status(404).end()
     }
+  })
+
+  const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+  
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      id: generateId(),
+    }
+  
+    notes = notes.concat(note)
+  
+    response.json(note)
   })
   
   app.delete('/api/notes/:id', (request, response) => {
