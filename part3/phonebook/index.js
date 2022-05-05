304ad36b99e8a90043fee71ae5d1a5e3fc625335
Gonzalo Gómez <gonzagom66@yahoo.com.ar>
2022-05-05 12:27:40 -0300

    TJ-8 3.6 finished

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -54,8 +54,15 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  if(!body.name)
-    response.status(404)
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number content missing' 
+    })
+  }
+  if(persons.find(person => person.name === body.name))
+    response.status(400).json({
+      error: 'name must be unique'
+    })
   
   const person = {
     name: body.name,
